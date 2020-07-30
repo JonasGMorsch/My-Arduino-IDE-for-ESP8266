85d164d32dd499e5d7eb6262f3f42eb7f46e0009
Jonas G. Morsch <jonasgmorsch@gmail.com>
2020-07-30 02:11:31 -0300

    esp8266 core 2.7.3 update

--- hardware/espressif/esp8266/libraries/Ethernet/src/EthernetServer.h
+++ hardware/espressif/esp8266/libraries/Ethernet/src/EthernetServer.h
@@ -1,3 +1,22 @@
-// This file is in the public domain.  No copyright is claimed.
+#ifndef ethernetserver_h
+#define ethernetserver_h
 
-#include "Ethernet.h"
+#include "Server.h"
+
+class EthernetClient;
+
+class EthernetServer : 
+public Server {
+private:
+  uint16_t _port;
+  void accept();
+public:
+  EthernetServer(uint16_t);
+  EthernetClient available();
+  virtual void begin();
+  virtual size_t write(uint8_t);
+  virtual size_t write(const uint8_t *buf, size_t size);
+  using Print::write;
+};
+
+#endif
